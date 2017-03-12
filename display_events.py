c2096712a12cfe29ef5bec60ff399f2fd48bbe1a
Max Schuman <maxschuman2018@u.northwestern.edu>
2017-03-12 10:21:27 -0500

    Added smaller test and training sound; added thresholding to display_events

--- display_events.py
+++ display_events.py
@@ -4,7 +4,7 @@ import numpy as np
 from globals import *
 
 
-def display_events(path, list_of_effects = LIST_OF_EFFECTS, moving_interval=10):
+def display_events(path, list_of_effects = LIST_OF_EFFECTS, moving_interval=10, threshold=None):
 	W, H = nmf_separate(path)
 	num_effects = len(list_of_effects)
 	consolidated_H = np.empty(shape=(num_effects, H.shape[1] - moving_interval + 1))
@@ -14,6 +14,10 @@ def display_events(path, list_of_effects = LIST_OF_EFFECTS, moving_interval=10):
 		# try the moving average of moving_interval values instead of just the sum
 		consolidated_H[i,:] = np.convolve(row, np.ones((moving_interval,))/moving_interval, mode='valid')
 		consolidated_H[i,:] = consolidated_H[i,:] / max(consolidated_H[i,:])
+
+		# binarize using threshold, if it is provided
+		if threshold is not None:
+			consolidated_H[i,:] = np.where(consolidated_H[i,:] > threshold, 1, 0)
 
 	# plot stuff
 	plt.figure(figsize=(18,20))
@@ -26,5 +30,5 @@ def display_events(path, list_of_effects = LIST_OF_EFFECTS, moving_interval=10):
 
 
 # test
-display_events('test_sounds/test_trained.wav')
+display_events('test_sounds/test_trained.wav', threshold=0.5)
 # display_events('test_sounds/test_trained_small.wav', list_of_effects=['Clear Throat', 'Cough', 'Door Slam'])
